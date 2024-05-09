94122e41c20de74c42c26e2ff05042bc11d5f151
Unknown <Unknown@DESKTOP-LAR4J4U>
2024-05-10 10:11:31 +1200

    Add project files.

---
+++ Menu/Models/DishIngredient.cs
@@ -0,0 +1,10 @@
+﻿namespace Menu.Models
+{
+    public class DishIngredient
+    {
+        public int DishId { get; set; }
+        public Dish Dishes { get; set; }
+        public int IngredientId { get; set; }
+        public Ingredient Ingredients { get; set; }
+    }
+}
